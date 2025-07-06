4d35d8efa83e722ec2c05a4a84891847d5f3452e
Kevin Klein <theambasadoracademy00@gmail.com>
2025-07-06 20:16:15 +0300

    Allow images from placehold.co and firebasestorage.googleapis.com

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   // ...other config...
   images: {
-    domains: [
-      'firebasestorage.googleapis.com',
-      'placehold.co',
-      // add any other domains you use for images
-    ],
+    domains: ['placehold.co', 'firebasestorage.googleapis.com'],
   },
   eslint: {
     ignoreDuringBuilds: true,
